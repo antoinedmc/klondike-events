119e5e2bf60798ac91bba27e45ab00ac5722d65b
antoinedmc <a.demonchaine@gmail.com>
2025-03-13 18:23:27 +0100

    Update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import localFont from "next/font/local";
+
 import ContactForm from "./_contact/_components/contact-form";
+
+const authenticSignatureFont = localFont({
+  src: "./fonts/Authentic Signature.otf",
+});
 
 export default function Home() {
   return (
     <main className="h-screen w-screen flex flex-col items-center justify-center">
       <div className="header">
-        <h1>The Castle</h1>
+        <h1 className={`${authenticSignatureFont.className}`}>The Castle</h1>
         <div className="flex justify-center">
           <span className="subtitle">KLONDIKE GROUP</span>
         </div>
